54afbe9b6f4f0e15c99a847f2dae57595728dc16
Ls <LINStear@outlook.com>
2019-11-22 16:03:13 +0800

    Bulid GeneratingContent Project

---
+++ Unity_Demo_GeneratingContent/GeneratingContent/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class MapGenerator : MonoBehaviour
+    {
+        #region Fields
+
+        public int Width;
+        public int Hight;
+
+        [Range(0,100)]
+        public int RandomFillPercent;
+
+        int[,] _map;
+        #endregion
+
+        #region MonoBehaiour Callbacks
+        private void Start()
+        {
+            _map = new int[Width, Hight];
+            GeneratorMap();
+        }
+
+        private void GeneratorMap()
+        {
+
+        }
+        #endregion
+
+    }
+
+}

73d5c42a2f94b56ca4ec79d543f597a86b6b27fe
Ls <LINStear@outlook.com>
2019-11-25 11:21:04 +0800

    MapGenerator completed

--- Unity_Demo_GeneratingContent/GeneratingContent/Assets/Scripts/MapGenerator.cs
+++ Unity_Demo_GeneratingContent/GeneratingContent/Assets/Scripts/MapGenerator.cs
@@ -12,9 +12,14 @@ namespace Game
 
         public int Width;
         public int Hight;
+        [Tooltip("自定义种子")]
         public string Seed;
+        [Tooltip("随机种子")]
         public bool UseRandomSeed;
-        [Range(0,100)] public int RandomFillPercent;
+        [Tooltip("平滑级别，将周围黑色相互融合，级别越高，融合次数越多")]
+        [Range(0, 10)] public int SmoothLevel = 5;
+        [Tooltip("黑色块占比")]
+        [Range(0, 100)] public int RandomFillPercent;
 
         int[,] _map;
 
@@ -23,8 +28,16 @@ namespace Game
         #region MonoBehaiour Callbacks
         private void Start()
         {
-            
             GeneratorMap();
+            
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                GeneratorMap();
+            }
         }
 
         private void OnDrawGizmos()
@@ -43,29 +56,79 @@ namespace Game
             }
         }
 
-
         #endregion
 
         #region Private Methods
-        private void GeneratorMap()
+        void GeneratorMap()
         {
             _map = new int[Width, Hight];
             RandomFillMap();
+            for (int i = 0; i < SmoothLevel; i++)
+            {
+                SmoothMap();
+            }
         }
 
-        private void RandomFillMap()
+        void RandomFillMap()
         {
             if (UseRandomSeed)
             {
                 Seed = Time.time.ToString();//伪随机取值以时间为种子
             }
-            Random pasudoRandom = new Random(Seed.GetHashCode());
+            Random pseuaoRandom = new Random(Seed.GetHashCode());
             for (int x = 0; x < Width; x++)
                 for (int y = 0; y < Hight; y++)
                 {
-                    //填充map，如果随机值大于填充百分比则作为墙否则就是空白
-                    _map[x, y] = pasudoRandom.Next(0, 100) < RandomFillPercent ? 1 : 0;
+                    if (x == 0 || x == Width - 1 || y == 0 || y == Hight - 1)
+                    {
+                        _map[x, y] = 1;
+                    }
+                    else
+                    {
+                        //填充map，如果随机值大于填充百分比则作为墙否则就是空白
+                        _map[x, y] = pseuaoRandom.Next(0, 100) < RandomFillPercent ? 1 : 0;
+                    }
                 }
+        }
+
+        void SmoothMap()
+        {
+            for (int x = 0; x < Width; x++)
+                for (int y = 0; y < Hight; y++)
+                {
+                    int neighbourCountTiles = GetSurrondWallCount(x, y);
+                    if (neighbourCountTiles > 4)
+                    {
+                        _map[x, y] = 1;
+                    }
+                    else if (neighbourCountTiles < 4)
+                        _map[x, y] = 0;
+                }
+        }
+
+        /// <summary>
+        /// 获取该tile周围八个的颜色情况（上下左右，左上左下右上右下）
+        /// </summary>
+        int GetSurrondWallCount(int girX, int girY)
+        {
+            int wallCount = 0;
+            for (int neighbourX = girX - 1; neighbourX <= girX + 1; neighbourX++)
+            {
+                for (int neighbourY = girY - 1; neighbourY <= girY + 1; neighbourY++)
+                {
+                    if (neighbourX >= 0 && neighbourX < Width && neighbourY >= 0 && neighbourY < Hight)
+                    {
+                        if (neighbourX != girX || neighbourY != girY)
+                        {
+                            wallCount += _map[neighbourX, neighbourY];
+                        }
+                    }
+                    else
+                        wallCount++;
+                }
+            }
+
+            return wallCount;
         }
         #endregion
     }

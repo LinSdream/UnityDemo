61b4339d8cfd31e44f0edb74228de32c0a700be5
Ls <LINStear@outlook.com>
2019-11-27 18:21:44 +0800

    bug查明，TriangulateSquare中321/421没有按照顺时针调用MeshFromPoints

--- Unity_Demo_GeneratingContent/GeneratingContent/Assets/Scripts/MeshGenerator.cs
+++ Unity_Demo_GeneratingContent/GeneratingContent/Assets/Scripts/MeshGenerator.cs
@@ -4,96 +4,6 @@ using UnityEngine;
 
 namespace Game
 {
-    #region Class Square
-    public class Square
-    {
-        public ControlNode TopLeft, TopRight, BottomLeft, BottomRight;
-        public Node CenterTop, CenterBottom, CenterRight, CenterLeft;
-        public int Configuration;//8个点的方块，三角网格配置有16种，即4位二进制0000 - 1111
-
-        public Square(ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomRight, ControlNode _bottomLeft)
-        {
-            TopLeft = _topLeft;
-            TopRight = _topRight;
-            BottomRight = _bottomRight;
-            BottomLeft = _bottomLeft;
-
-            CenterTop = TopLeft.RightNode;
-            CenterRight = BottomRight.AboveNode;
-            CenterBottom = BottomLeft.RightNode;
-            CenterLeft = BottomLeft.AboveNode;
-
-            if (TopLeft.Active)
-                Configuration += 8;
-            if (TopRight.Active)
-                Configuration += 4;
-            if (BottomRight.Active)
-                Configuration += 2;
-            if (BottomLeft.Active)
-                Configuration += 1;
-        }
-
-
-        //public Square(ControlNode bottomLeft, ControlNode bottomRight, ControlNode topLeft, ControlNode topRight)
-        //{
-        //    TopLeft = topLeft;
-        //    TopRight = topRight;
-        //    BottomLeft = bottomLeft;
-        //    BottomRight = bottomRight;
-
-        //    CenterTop = TopLeft.RightNode;
-        //    CenterRight = BottomRight.AboveNode;
-        //    CenterBottom = BottomLeft.RightNode;
-        //    CenterLeft = TopLeft.AboveNode;
-
-        //    if (TopLeft.Active)
-        //        Configuration += 8;
-        //    if (TopRight.Active)
-        //        Configuration += 4;
-        //    if (BottomRight.Active)
-        //        Configuration += 2;
-        //    if (BottomLeft.Active)
-        //        Configuration += 1;
-        //}
-    }
-    #endregion
-
-    #region Class SquareGird
-    public class SquareGird
-    {
-        public Square[,] Squares;
-
-        public SquareGird(int [,] map,float squareSize)
-        {
-            int nodeCountX = map.GetLength(0);//x轴node数目
-            int nodeCountY = map.GetLength(1);//y轴node数量
-
-            float mapWidth = nodeCountX * squareSize;//map宽度
-            float mapHeight = nodeCountY * squareSize;//map高度
-
-            ControlNode[,] controlNodes = new ControlNode[nodeCountX, nodeCountY];
-            for (int x = 0; x < nodeCountX; x++)
-            {
-                for (int y = 0; y < nodeCountY; y++)
-                {
-                    //计算控制点的中点
-                    Vector3 pos = new Vector3(-mapWidth / 2 + x * squareSize + squareSize / 2, 0, -mapHeight / 2 + y * squareSize + squareSize / 2);
-                    controlNodes[x, y] = new ControlNode(pos, map[x, y] == 1, squareSize);   
-                }
-            }
-            Squares = new Square[nodeCountX - 1, nodeCountY - 1];
-            for(int x = 0; x < nodeCountX - 1; x++)
-            {
-                for(int y = 0; y < nodeCountY - 1; y++)
-                {
-                    //public Square(ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomRight, ControlNode _bottomLeft)
-                    Squares[x, y] = new Square(controlNodes[x, y + 1], controlNodes[x + 1, y + 1], controlNodes[x + 1, y], controlNodes[x, y]);
-                    //Squares[x, y] = new Square(controlNodes[x, y], controlNodes[x+1, y], controlNodes[x, y+1], controlNodes[x + 1, y + 1]);
-                }
-            }
-        }
-    }
-    #endregion
     public class MeshGenerator : MonoBehaviour
     {
         #region Fields
@@ -155,128 +65,69 @@ namespace Game
         void TriangulateSquare(Square square)
         {
 
-            switch (square.Configuration)
-            {
-                case 0:
-                    break;
-
-                // 1 points:
-                case 1:
-                    MeshFromPoints(square.CenterBottom, square.BottomLeft, square.CenterLeft);
-                    break;
-                case 2:
-                    MeshFromPoints(square.CenterRight, square.BottomRight, square.CenterBottom);
-                    break;
-                case 4:
-                    MeshFromPoints(square.CenterTop, square.TopRight, square.CenterRight);
-                    break;
-                case 8:
-                    MeshFromPoints(square.TopLeft, square.CenterTop, square.CenterLeft);
-                    break;
-
-                // 2 points:
-                case 3:
-                    MeshFromPoints(square.CenterRight, square.BottomRight, square.BottomLeft, square.CenterLeft);
-                    break;
-                case 6:
-                    MeshFromPoints(square.CenterTop, square.TopRight, square.BottomRight, square.CenterBottom);
-                    break;
-                case 9:
-                    MeshFromPoints(square.TopLeft, square.CenterTop, square.CenterBottom, square.BottomLeft);
-                    break;
-                case 12:
-                    MeshFromPoints(square.TopLeft, square.TopRight, square.CenterRight, square.CenterLeft);
-                    break;
-                case 5:
-                    MeshFromPoints(square.CenterTop, square.TopRight, square.CenterRight, square.CenterBottom, square.BottomLeft, square.CenterLeft);
-                    break;
-                case 10:
-                    MeshFromPoints(square.TopLeft, square.CenterTop, square.CenterRight, square.BottomRight, square.CenterBottom, square.CenterLeft);
-                    break;
-
-                // 3 point:
-                case 7:
-                    MeshFromPoints(square.CenterTop, square.TopRight, square.BottomRight, square.BottomLeft, square.CenterLeft);
-                    break;
-                case 11:
-                    MeshFromPoints(square.TopLeft, square.CenterTop, square.CenterRight, square.BottomRight, square.BottomLeft);
-                    break;
-                case 13:
-                    MeshFromPoints(square.TopLeft, square.TopRight, square.CenterRight, square.CenterBottom, square.BottomLeft);
-                    break;
-                case 14:
-                    MeshFromPoints(square.TopLeft, square.TopRight, square.BottomRight, square.CenterBottom, square.CenterLeft);
-                    break;
-
-                // 4 point:
-                case 15:
-                    MeshFromPoints(square.TopLeft, square.TopRight, square.BottomRight, square.BottomLeft);
-                    break;
-            }
-
             //参考
             //1   A   2
             //B   *   C
             //3   D   4
-            //switch (square.Configuration)
-            //{
-            //    //No points
-            //    case 0://0000
-            //        break;
+            switch (square.Configuration)
+            {
+                //No points
+                case 0://0000
+                    break;
 
-            //    // 1 point 
-            //    case 1://0001   3
-            //        MeshFromPoints(square.CenterBottom, square.BottomLeft, square.CenterLeft);//D3B
-            //        break;
-            //    case 2://0010   4
-            //        MeshFromPoints(square.CenterRight, square.BottomRight, square.CenterBottom);//C4D
-            //        break;
-            //    case 4://0100   2
-            //        MeshFromPoints(square.CenterTop, square.TopRight, square.CenterRight);//A2C
-            //        break;
-            //    case 8://1000   1
-            //        MeshFromPoints(square.TopLeft, square.CenterTop, square.CenterLeft);//1AB
-            //        break;
+                // 1 point 
+                case 1://0001   3
+                    MeshFromPoints(square.CenterBottom, square.BottomLeft, square.CenterLeft);//D3B
+                    break;
+                case 2://0010   4
+                    MeshFromPoints(square.CenterRight, square.BottomRight, square.CenterBottom);//C4D
+                    break;
+                case 4://0100   2
+                    MeshFromPoints(square.CenterTop, square.TopRight, square.CenterRight);//A2C
+                    break;
+                case 8://1000   1
+                    MeshFromPoints(square.TopLeft, square.CenterTop, square.CenterLeft);//1AB
+                    break;
 
-            //    //2 points
-            //    case 3://0011 34
-            //        MeshFromPoints(square.CenterRight, square.BottomRight, square.BottomLeft, square.CenterLeft);//BC43
-            //        break;
-            //    case 6://0110 24
-            //        MeshFromPoints(square.CenterTop, square.TopRight, square.BottomRight, square.CenterBottom);//A24D
-            //        break;
-            //    case 9://1001 31
-            //        MeshFromPoints(square.TopLeft, square.CenterTop, square.CenterBottom, square.BottomLeft);//1AD3
-            //        break;
-            //    case 12://1100 12
-            //        MeshFromPoints(square.TopLeft, square.TopRight, square.CenterRight, square.CenterLeft);//12CB
-            //        break;
-            //    case 5://0101 32
-            //        MeshFromPoints(square.CenterTop, square.TopRight, square.CenterRight, square.CenterBottom,square.BottomLeft,square.CenterLeft);//A2CD3B
-            //        break;
-            //    case 10://1010 41
-            //        MeshFromPoints(square.TopLeft, square.CenterLeft, square.CenterBottom, square.BottomRight, square.CenterRight, square.CenterTop);//1BD4CA
-            //        break;
+                //2 points
+                case 3://0011 34
+                    MeshFromPoints(square.CenterRight, square.BottomRight, square.BottomLeft, square.CenterLeft);//BC43
+                    break;
+                case 6://0110 24
+                    MeshFromPoints(square.CenterTop, square.TopRight, square.BottomRight, square.CenterBottom);//A24D
+                    break;
+                case 9://1001 31
+                    MeshFromPoints(square.TopLeft, square.CenterTop, square.CenterBottom, square.BottomLeft);//1AD3
+                    break;
+                case 12://1100 12
+                    MeshFromPoints(square.TopLeft, square.TopRight, square.CenterRight, square.CenterLeft);//12CB
+                    break;
+                case 5://0101 32
+                    MeshFromPoints(square.CenterTop, square.TopRight, square.CenterRight, square.CenterBottom, square.BottomLeft, square.CenterLeft);//A2CD3B
+                    break;
+                case 10://1010 41
+                    MeshFromPoints(square.TopLeft, square.CenterTop, square.CenterRight, square.BottomRight, square.CenterBottom, square.CenterLeft);//1AC4DB
+                    break;
 
-            //    //3 points
-            //    case 7://0111   342
-            //        MeshFromPoints(square.CenterTop, square.TopRight, square.BottomRight, square.BottomLeft,square.CenterLeft);//B342A
-            //        break;
-            //    case 11://1011   341
-            //        MeshFromPoints(square.TopLeft, square.CenterTop, square.CenterRight, square.BottomRight, square.BottomLeft);//1AC43
-            //        break;
-            //    case 13://1101   321
-            //        MeshFromPoints(square.TopLeft, square.BottomLeft, square.CenterBottom, square.CenterRight, square.TopRight);//13DC2
-            //        break;
-            //    case 14://1110   421
-            //        MeshFromPoints(square.TopLeft, square.CenterLeft, square.CenterBottom, square.BottomRight, square.TopRight);//1BD42
-            //        break;
+                //3 points
+                case 7://0111   342
+                    MeshFromPoints(square.CenterTop, square.TopRight, square.BottomRight, square.BottomLeft, square.CenterLeft);//A243B
+                    break;
+                case 11://1011   341
+                    MeshFromPoints(square.TopLeft, square.CenterTop, square.CenterRight, square.BottomRight, square.BottomLeft);//1AC43
+                    break;
+                case 13://1101   321
+                    MeshFromPoints(square.TopLeft, square.TopRight, square.CenterRight, square.CenterBottom, square.BottomLeft);//12CD3
+                    break;
+                case 14://1110   421
+                    MeshFromPoints(square.TopLeft, square.TopRight, square.BottomRight, square.CenterBottom, square.CenterLeft);//124DB
+                    break;
 
-            //    //4 points
-            //    case 15://1111  1234
-            //        MeshFromPoints(square.TopLeft, square.TopRight, square.BottomRight, square.BottomLeft);//1234
-            //        break;
-            //}
+                //4 points
+                case 15://1111  1243
+                    MeshFromPoints(square.TopLeft, square.TopRight, square.BottomRight, square.BottomLeft);//1243
+                    break;
+            }
         }
 
         /// <summary>
---
+++ Unity_Demo_GeneratingContent/GeneratingContent/Assets/Scripts/SquareGird.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Game
+{
+    public class Square
+    {
+        public ControlNode TopLeft, TopRight, BottomLeft, BottomRight;
+        public Node CenterTop, CenterBottom, CenterRight, CenterLeft;
+        public int Configuration;//8个点的方块，三角网格配置有16种，即4位二进制0000 - 1111
+
+        public Square(ControlNode topLeft, ControlNode topRight, ControlNode bottomRight, ControlNode bottomLeft)
+        {
+            TopLeft = topLeft;
+            TopRight = topRight;
+            BottomRight = bottomRight;
+            BottomLeft = bottomLeft;
+
+            CenterTop = TopLeft.RightNode;
+            CenterRight = BottomRight.AboveNode;
+            CenterBottom = BottomLeft.RightNode;
+            CenterLeft = BottomLeft.AboveNode;
+
+            if (TopLeft.Active)
+                Configuration += 8;
+            if (TopRight.Active)
+                Configuration += 4;
+            if (BottomRight.Active)
+                Configuration += 2;
+            if (BottomLeft.Active)
+                Configuration += 1;
+        }
+    }
+
+    public class SquareGird
+    {
+        public Square[,] Squares;
+
+        public SquareGird(int[,] map, float squareSize)
+        {
+            int nodeCountX = map.GetLength(0);//x轴node数目
+            int nodeCountY = map.GetLength(1);//y轴node数量
+
+            float mapWidth = nodeCountX * squareSize;//map宽度
+            float mapHeight = nodeCountY * squareSize;//map高度
+
+            ControlNode[,] controlNodes = new ControlNode[nodeCountX, nodeCountY];
+            for (int x = 0; x < nodeCountX; x++)
+            {
+                for (int y = 0; y < nodeCountY; y++)
+                {
+                    //计算控制点的中点
+                    Vector3 pos = new Vector3(-mapWidth / 2 + x * squareSize + squareSize / 2, 0, -mapHeight / 2 + y * squareSize + squareSize / 2);
+                    controlNodes[x, y] = new ControlNode(pos, map[x, y] == 1, squareSize);
+                }
+            }
+            Squares = new Square[nodeCountX - 1, nodeCountY - 1];
+            for (int x = 0; x < nodeCountX - 1; x++)
+            {
+                for (int y = 0; y < nodeCountY - 1; y++)
+                {
+                    Squares[x, y] = new Square(controlNodes[x, y + 1], controlNodes[x + 1, y + 1], controlNodes[x + 1, y], controlNodes[x, y]);
+                }
+            }
+        }
+    }
+}

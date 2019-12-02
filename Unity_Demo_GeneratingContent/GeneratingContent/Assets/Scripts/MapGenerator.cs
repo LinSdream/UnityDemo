1dfbafd3cbb755724b77f4e8d9b6c0ac10edef67
Ls <LINStear@outlook.com>
2019-12-02 17:19:20 +0800

    version upgrade 2019.3.0f1 bug : the map error

--- Unity_Demo_GeneratingContent/GeneratingContent/Assets/Scripts/MapGenerator.cs
+++ Unity_Demo_GeneratingContent/GeneratingContent/Assets/Scripts/MapGenerator.cs
@@ -12,7 +12,7 @@ namespace Game
         public int TileX;
         public int TileY;
 
-        public Coord(int x,int y)
+        public Coord(int x, int y)
         {
             TileX = x;
             TileY = y;
@@ -36,7 +36,8 @@ namespace Game
         [Range(0, 100)] public int RandomFillPercent;
         [Tooltip("边缘轮廓尺寸")]
         public int BorderSize = 5;
-        
+        public int WallThresholdSize = 50;
+
         int[,] _map;
 
         #endregion
@@ -68,17 +69,18 @@ namespace Game
                 SmoothMap();
             }
 
-            
+           // ProcessMap();
+
             int[,] borderMap = new int[Width + BorderSize * 2, Hight + BorderSize * 2];
 
             int borderX = borderMap.GetLength(0);
             int borderY = borderMap.GetLength(1);
 
-            for(int x = 0; x < borderX; x++)
+            for (int x = 0; x < borderX; x++)
             {
-                for(int y = 0; y < borderY; y++)
+                for (int y = 0; y < borderY; y++)
                 {
-                    if((x >= BorderSize && x < BorderSize + Width) && (y >= BorderSize && y < BorderSize + Hight))
+                    if ((x >= BorderSize && x < BorderSize + Width) && (y >= BorderSize && y < BorderSize + Hight))
                     {
                         borderMap[x, y] = _map[x - BorderSize, y - BorderSize];
                     }
@@ -140,7 +142,7 @@ namespace Game
             {
                 for (int neighbourY = girY - 1; neighbourY <= girY + 1; neighbourY++)
                 {
-                    if (IsInWallRange(neighbourX,neighbourY))
+                    if (IsInWallRange(neighbourX, neighbourY))
                     {
                         if (neighbourX != girX || neighbourY != girY)
                         {
@@ -154,7 +156,7 @@ namespace Game
             return wallCount;
         }
 
-        List<Coord> GetRegionTiles(int startX,int startY)
+        List<Coord> GetRegionTiles(int startX, int startY)
         {
             List<Coord> tiles = new List<Coord>();
             Queue<Coord> queue = new Queue<Coord>();
@@ -165,20 +167,20 @@ namespace Game
             //第一个点进入
             queue.Enqueue(new Coord(startX, startY));
             _map[startX, startY] = 1;
-            
-            while(queue.Count>0)
+
+            while (queue.Count > 0)
             {
                 Coord tile = queue.Dequeue();
                 tiles.Add(tile);
 
-                for(int x=tile.TileX-1;x<=tile.TileX+1;x++)
+                for (int x = tile.TileX - 1; x <= tile.TileX + 1; x++)
                 {
-                    for(int y=tile.TileY-1;y<=tile.TileY+1;y++)
+                    for (int y = tile.TileY - 1; y <= tile.TileY + 1; y++)
                     {
                         //只需要上下左右四个点的贴图信息，因此必然存在一个x or y 等于该点的x or y
                         if (IsInWallRange(x, y) && (y == tile.TileY || x == tile.TileX))
                         {
-                            if(mapFlags[x,y]==0&&mapFlags[x,y]==tileType)
+                            if (mapFlags[x, y] == 0 && mapFlags[x, y] == tileType)
                             {
                                 mapFlags[x, y] = 1;
                                 queue.Enqueue(new Coord(x, y));
@@ -190,9 +192,49 @@ namespace Game
             return tiles;
         }
 
-        bool IsInWallRange(int x,int y)
+        List<List<Coord>> GetRegions(int tileType)
         {
-            return (x >= 0 && x < Width) && (y >- 0 && y < Hight);
+            List<List<Coord>> regions = new List<List<Coord>>();
+            int[,] mapFlags = new int[Width, Hight];
+
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Hight; y++)
+                {
+                    if (mapFlags[x, y] == 0 && _map[x, y] == tileType)
+                    {
+                        List<Coord> newRegions = GetRegionTiles(x, y);
+                        regions.Add(newRegions);
+
+                        foreach (Coord cell in newRegions)
+                        {
+                            mapFlags[cell.TileX, cell.TileY] = 1;
+                        }
+                    }
+                }
+            }
+            return regions;
+        }
+
+        void ProcessMap()
+        {
+            List<List<Coord>> wallRegions = GetRegions(1);
+
+            foreach(List<Coord> cell in wallRegions)
+            {
+                if (cell.Count < WallThresholdSize)
+                {
+                    foreach(Coord tile in cell)
+                    {
+                        _map[tile.TileX, tile.TileY] = 0;
+                    }
+                }
+            }
+        }
+
+        bool IsInWallRange(int x, int y)
+        {
+            return (x >= 0 && x < Width) && (y > -0 && y < Hight);
         }
 
         #endregion

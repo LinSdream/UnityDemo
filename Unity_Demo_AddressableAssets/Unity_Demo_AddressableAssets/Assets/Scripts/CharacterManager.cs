8895d5b458ebc32dfed022f0aee1da5bda6ef6f8
Ls <LINStear@outlook.com>
2019-12-09 17:27:27 +0800

    Towers assets and tow or more labels to loadAssets

--- Unity_Demo_AddressableAssets/Unity_Demo_AddressableAssets/Assets/Scripts/CharacterManager.cs
+++ Unity_Demo_AddressableAssets/Unity_Demo_AddressableAssets/Assets/Scripts/CharacterManager.cs
@@ -5,54 +5,114 @@ using UnityEngine.ResourceManagement.AsyncOperations; // TODO: Mention that this
 
 public class CharacterManager : MonoBehaviour
 {
-    public GameObject m_archerObject;
+    #region Fields
+    public List<AssetReference> Characters;//AddressAssets对象列表
 
-    //public AssetReference m_ArcherObject;
+    bool _isAssetsReady = false;//异步加载，资源是否加载完成
+    int _charactorsAssetsCount;//加载资源的数量，用于辅助判断IsAssetsReady是否完成
+    #endregion
 
-    //public List<AssetReference> m_Characters;
-    //bool m_AssetsReady = false;
-    //int m_ToLoadCount;
-    //int m_CharacterIndex = 0;
-
-    // Start is called before the first frame update
-    void Start()
+    #region MonoBehaviour Callbacks
+    private void Start()
     {
-        //m_ToLoadCount = m_Characters.Count;
+        _charactorsAssetsCount = Characters.Count;
+        
+        //LoadAssets
+        foreach(AssetReference cell in Characters)
+        {
+            //如果当前加载资源完成，进行回调
+            cell.LoadAssetAsync<GameObject>().Completed += OnCharacterAssetLoaded;
+        }
 
-        //foreach (var character in m_Characters)
-        //{
-        //    character.LoadAssetAsync<GameObject>().Completed += OnCharacterAssetLoaded;
-        //}
     }
 
+    private void OnDestroy()
+    {
+        foreach(AssetReference cell in Characters)
+        {
+            cell.ReleaseAsset();
+        }
+    }
 
+    #endregion
+
+    #region Callbacks Events
+    void OnCharacterAssetLoaded(AsyncOperationHandle<GameObject> obj)
+    {
+        //所有待加载资源数--
+        _charactorsAssetsCount--;
+        //如果所有资源加载完成
+        if (_charactorsAssetsCount <= 0)
+            _isAssetsReady = true;//解锁
+    }
+    #endregion
+
+    #region Buttons
+    //Instantiate assets
     public void SpawnCharacter(int characterType)
     {
-        Instantiate(m_archerObject);
-
-        //m_ArcherObject.InstantiateAsync();
-
-        //if (m_AssetsReady)
-        //{
-        //    Vector3 position = Random.insideUnitSphere * 5;
-        //    position.Set(position.x, 0, position.z);
-        //    m_Characters[characterType].InstantiateAsync(position, Quaternion.identity);
-        //}
+        //只有所有资源加载完成后，才能进行实例化
+        if(_isAssetsReady)
+        {
+            Vector3 pos = Random.insideUnitSphere * 5;
+            pos.Set(pos.x, 0, pos.z);
+            Characters[characterType].InstantiateAsync(pos, Quaternion.identity);//异步实例化
+        }
     }
+    #endregion
 
-    //void OnCharacterAssetLoaded(AsyncOperationHandle<GameObject> obj)
+
+    #region Completed
+
+    //public GameObject m_archerObject;
+
+    ////public AssetReference m_ArcherObject;
+
+    ////public List<AssetReference> m_Characters;
+    ////bool m_AssetsReady = false;
+    ////int m_ToLoadCount;
+    ////int m_CharacterIndex = 0;
+
+    //// Start is called before the first frame update
+    //void Start()
     //{
-    //    m_ToLoadCount--;
+    //    //m_ToLoadCount = m_Characters.Count;
 
-    //    if (m_ToLoadCount <= 0)
-    //        m_AssetsReady = true;
+    //    //foreach (var character in m_Characters)
+    //    //{
+    //    //    character.LoadAssetAsync<GameObject>().Completed += OnCharacterAssetLoaded;
+    //    //}
     //}
 
-    //private void OnDestroy() //TODO: Should we teach instantiate with game objects and then manually release?
+
+    //public void SpawnCharacter(int characterType)
     //{
-    //    foreach (var character in m_Characters)
-    //    {
-    //        character.ReleaseAsset();
-    //    }
+    //    Instantiate(m_archerObject);
+
+    //    //m_ArcherObject.InstantiateAsync();
+
+    //    //if (m_AssetsReady)
+    //    //{
+    //    //    Vector3 position = Random.insideUnitSphere * 5;
+    //    //    position.Set(position.x, 0, position.z);
+    //    //    m_Characters[characterType].InstantiateAsync(position, Quaternion.identity);
+    //    //}
     //}
+
+    ////void OnCharacterAssetLoaded(AsyncOperationHandle<GameObject> obj)
+    ////{
+    ////    m_ToLoadCount--;
+
+    ////    if (m_ToLoadCount <= 0)
+    ////        m_AssetsReady = true;
+    ////}
+
+    ////private void OnDestroy() //TODO: Should we teach instantiate with game objects and then manually release?
+    ////{
+    ////    foreach (var character in m_Characters)
+    ////    {
+    ////        character.ReleaseAsset();
+    ////    }
+    ////}
+    #endregion
 }
--- Unity_Demo_AddressableAssets/Unity_Demo_AddressableAssets/Assets/Scripts/TowerManager.cs
+++ Unity_Demo_AddressableAssets/Unity_Demo_AddressableAssets/Assets/Scripts/TowerManager.cs
@@ -8,36 +8,75 @@ using UnityEngine.UI;
 
 public class TowerManager : MonoBehaviour
 {
-    public IList<GameObject> m_Towers;
+    #region Fileds
+    public List<AssetLabelReference> AssetLabels;
+    public IList<GameObject> Towers;
+    public Button[] TowerCards;
 
-    public AssetLabelReference m_TowerLabel;
+    #endregion
 
-    public Button[] m_TowerCards;
-
-    // Start is called before the first frame update
-    void Start()
+    #region MonoBehaviour Callbacks
+    private void Start()
     {
-        Addressables.LoadAssetsAsync<GameObject>(m_TowerLabel, null).Completed += OnResourcesRetrieved;
+        ///TODO:研究多标签loadassets
+        //Addressables.LoadAssetsAsync<GameObject>()
+        Addressables.LoadAssetsAsync<GameObject>(AssetLabels, null).Completed += OnLoadCompleted;
     }
 
-    private void OnResourcesRetrieved(AsyncOperationHandle<IList<GameObject>> obj)
+    private void OnLoadCompleted(AsyncOperationHandle<IList<GameObject>> obj)
     {
-        m_Towers = obj.Result;
-
-        //Activate the tower cards since their assets are now loaded
-        foreach(var towerCard in m_TowerCards)
+        Towers = obj.Result;
+        foreach(var cell in TowerCards)
         {
-            towerCard.interactable = true;
+            cell.interactable = true;
         }
     }
+    #endregion
 
+    #region Buttons
     public void InstantiateTower(int index)
     {
-        if(m_Towers != null)
+        if(Towers!=null)
         {
-            Vector3 position = Random.insideUnitSphere * 5;
-            position.Set(position.x, 0, position.z);
-            Instantiate(m_Towers[index], position, Quaternion.identity, null);
+            Vector3 pos = Random.insideUnitSphere * 5;
+            pos.Set(pos.x, 0, pos.z);
+            Instantiate(Towers[index],pos, Quaternion.identity);
         }
     }
+    #endregion
+
+    #region Completed
+    //public IList<GameObject> m_Towers;
+
+    //public AssetLabelReference m_TowerLabel;
+
+    //public Button[] m_TowerCards;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    Addressables.LoadAssetsAsync<GameObject>(m_TowerLabel, null).Completed += OnResourcesRetrieved;
+    //}
+
+    //private void OnResourcesRetrieved(AsyncOperationHandle<IList<GameObject>> obj)
+    //{
+    //    m_Towers = obj.Result;
+
+    //    //Activate the tower cards since their assets are now loaded
+    //    foreach(var towerCard in m_TowerCards)
+    //    {
+    //        towerCard.interactable = true;
+    //    }
+    //}
+
+    //public void InstantiateTower(int index)
+    //{
+    //    if(m_Towers != null)
+    //    {
+    //        Vector3 position = Random.insideUnitSphere * 5;
+    //        position.Set(position.x, 0, position.z);
+    //        Instantiate(m_Towers[index], position, Quaternion.identity, null);
+    //    }
+    //}
+    #endregion
 }

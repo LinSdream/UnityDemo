bb39689c5e58f94c9647937880ce57d05088bf53
Ls <LINStear@outlook.com>
2019-12-24 10:34:57 +0800

    PlayerAnimManager code.

--- Unity_Demo_ImitateDarkSoul/Unity_Demo_ImitateDarkSoul/Assets/Scripts/Characters/Player/PlayerAnimManager.cs
+++ Unity_Demo_ImitateDarkSoul/Unity_Demo_ImitateDarkSoul/Assets/Scripts/Characters/Player/PlayerAnimManager.cs
@@ -20,6 +20,8 @@ namespace Game
         #endregion
 
         #region Public Fields
+
+        #region AnimatorClip Name
         public string Attack01;
         public string Attack02;
         public string Attack03;
@@ -32,19 +34,33 @@ namespace Game
         public string Unsheath;
         #endregion
 
+        #region Animator Parameters
+        public string Parameters_PlayerActionCMD = "ActionCMD";
+        public string Parameters_Running;
+        public string Parameters_SpecialAttack;
+        public string Parameters_SwitchWeapon;
+        #endregion
+
+        #region Player Default Animator Clip Name
+        public string Drink = "Player_Drinking";
+        public string EmptyDrink = "Player_EmptyDrinking";
+        public string Sitting = "Player_Sitting";
+        #endregion
+        #endregion
+
         #region Public Methods
         public void GetAnimByWeaponName(string name)
         {
-            Attack01 = name + "Attack01";
-            Attack02 = name + "Attack02";
-            Attack03 = name + "Attack03";
-            Death = name + "Death";
-            GetHit = name + "GetHit";
-            Idle = name + "Idle";
-            Roll = name + "Roll";
-            Run = name + "Run";
-            SpecialAttack = name + "SpecialAttack";
-            Unsheath = name + "Unsheath";
+            Attack01 = name + "-Attack01";
+            Attack02 = name + "-Attack02";
+            Attack03 = name + "-Attack03";
+            Death = name + "-Death";
+            GetHit = name + "-GetHit";
+            Idle = name + "-Idle";
+            Roll = name + "-Roll";
+            Run = name + "-Run";
+            SpecialAttack = name + "-SpecialAttack";
+            Unsheath = name + "-Unsheath";
         }
         #endregion
     }

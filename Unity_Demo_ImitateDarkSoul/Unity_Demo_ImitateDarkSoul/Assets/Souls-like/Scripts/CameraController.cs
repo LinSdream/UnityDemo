dfe0dd08d2a4b4cd0a344fd89e00263ca9f9fdcb
Ls <LINStear@outlook.com>
2019-12-31 14:33:43 +0800

    Camera Controller

---
+++ Unity_Demo_ImitateDarkSoul/Unity_Demo_ImitateDarkSoul/Assets/Souls-like/Scripts/CameraController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Souls
+{
+    public class CameraController : MonoBehaviour
+    {
+        public PlayerInput _input;
+
+        [Header("Inversion")]
+        public bool HorizontalInversion = false;
+        public bool VerticalInversion = false;
+
+        [Header("CameraRotationSpeed")]
+        public float HorizontalSpeed;
+        public float VerticalSpeed;
+
+        [Header("Limit Angle,x:min , y:max")]
+        public Vector2 VerticalAngle;
+
+        Transform VerticalAxis;
+        Transform HorizontalAxis;
+        float _tmpEulerX = 0;
+        Transform _model;
+
+        private void Awake()
+        {
+            VerticalAxis = transform.parent;
+            HorizontalAxis = transform.parent.parent;
+
+            _model = _input.GetComponent<PlayerController>().Model.transform;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (HorizontalInversion)
+                HorizontalSpeed = -HorizontalSpeed;
+            else
+                HorizontalSpeed = Mathf.Abs(HorizontalSpeed);
+            if (VerticalInversion)
+                VerticalSpeed = -VerticalSpeed;
+            else
+                VerticalSpeed = Mathf.Abs(VerticalSpeed);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            Vector3 modelEuler = _model.eulerAngles;
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+                _input.LockCursor = !_input.LockCursor;
+
+            _tmpEulerX -= _input.CameraVertical * VerticalSpeed * Time.deltaTime;
+            _tmpEulerX = Mathf.Clamp(_tmpEulerX, VerticalAngle.x, VerticalAngle.y);
+
+            VerticalAxis.localEulerAngles = new Vector3(_tmpEulerX, 0, 0);
+            HorizontalAxis.Rotate(Vector3.up, _input.CameraHorizontal * HorizontalSpeed * Time.deltaTime);
+            //VerticalAxis.Rotate(Vector3.right, _input.CameraVertical * VerticalSpeed * Time.deltaTime);
+
+            _model.eulerAngles = modelEuler; 
+        }
+
+        public void Inversion(bool horizontal,bool vertical)
+        {
+            if (horizontal)
+                HorizontalSpeed = -HorizontalSpeed;
+            else
+                HorizontalSpeed = Mathf.Abs(HorizontalSpeed);
+            if (vertical)
+                VerticalSpeed = -VerticalSpeed;
+            else
+                VerticalSpeed = Mathf.Abs(VerticalSpeed);
+        }
+    }
+
+}
--- Unity_Demo_ImitateDarkSoul/Unity_Demo_ImitateDarkSoul/Assets/Souls-like/Scripts/Characters/PlayerInput.cs
+++ Unity_Demo_ImitateDarkSoul/Unity_Demo_ImitateDarkSoul/Assets/Souls-like/Scripts/Characters/PlayerInput.cs
@@ -11,13 +11,19 @@ namespace Souls
         public string HorizontalAxis = "Horizontal";
         public string VerticalAxis = "VerticalAxis";
 
+        public string CameraHorizontalAxis;
+        public string CameraVerticalAxis;
+
         [Header("Buttons")]
         public string RunButton = "Run";
-        public string Jump = "Jump";
+        public string JumpButton = "Jump";
+        public string LockCursorButton = "Esc";
 
-        //input signal
+        //output signal
         [HideInInspector] public float Horizontal;
         [HideInInspector] public float Vertical;
+        [HideInInspector] public float CameraHorizontal;
+        [HideInInspector] public float CameraVertical;
 
         //pressing signal
         [HideInInspector] public bool IsRun;
@@ -25,16 +31,37 @@ namespace Souls
         //trigger signal
         [HideInInspector] public bool IsJump = false;
 
+        //others
+        [HideInInspector] public bool LockCursor = true;
         [HideInInspector] public Vector3 InputVec => new Vector3(Horizontal, 0, Vertical);
+
+        private void Awake()
+        {
+            IsRun = false;
+            IsJump = false;
+            LockCursor = true;
+        }
 
         void Update()
         {
+
+            if (Input.GetButtonDown(LockCursorButton))
+                LockCursor = !LockCursor;
+
+            if(LockCursor)
+            {
+                CameraHorizontal = Input.GetAxis(CameraHorizontalAxis);
+                CameraVertical = Input.GetAxis(CameraVerticalAxis);
+            }
+
+            Debug.Log(new Vector3(CameraHorizontal, 0, CameraVertical));
+
             Horizontal = Input.GetAxis(HorizontalAxis);
             Vertical = Input.GetAxis(VerticalAxis);
 
             IsRun = Input.GetButton(RunButton);
 
-            IsJump = Input.GetButtonDown(Jump);
+            IsJump = Input.GetButtonDown(JumpButton);
 
         }
     }

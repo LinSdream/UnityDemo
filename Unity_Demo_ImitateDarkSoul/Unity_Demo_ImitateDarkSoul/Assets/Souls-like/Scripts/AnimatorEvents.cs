dd85dff1bc372d51c4a5624819cf1595b6792ac4
Ls <LINStear@outlook.com>
2020-02-27 22:59:52 +0800

    修复Weapon Collider在动画期间重复碰撞bug， bug：玩家受击时候，无法移动

--- Unity_Demo_ImitateDarkSoul/Unity_Demo_ImitateDarkSoul/Assets/Souls-like/Scripts/AnimatorEvents.cs
+++ Unity_Demo_ImitateDarkSoul/Unity_Demo_ImitateDarkSoul/Assets/Souls-like/Scripts/AnimatorEvents.cs
@@ -7,17 +7,45 @@ namespace Souls
     public class AnimatorEvents : MonoBehaviour
     {
 
+        public string WeaponUrl = "mixamorig:Hips/mixamorig:Spine/mixamorig:Spine1/mixamorig:Spine2/mixamorig:RightShoulder/mixamorig:RightArm/mixamorig:RightForeArm/mixamorig:RightHand/WeaponHandle";
+
+        GameObject Weapon;
+        Collider WeaponCollider;
         Animator _anim;
 
         private void Awake()
         {
             _anim = GetComponent<Animator>();
+            Weapon = gameObject.transform.Find(WeaponUrl).gameObject;
+            WeaponCollider = Weapon.GetComponentsInChildren<Collider>()[0];
         }
 
+        #region Public Methods
+        public Collider ChangeWeaponCollider(int index)
+        {
+            WeaponCollider = Weapon.GetComponentsInChildren<Collider>()[index];
+            if (WeaponCollider == null)
+                WeaponCollider = ChangeWeaponCollider(0);
+            return WeaponCollider;
+        }
+        #endregion
+
+        #region AnimatorEvents
         public void ResetTrigger(string name)
         {
             _anim.ResetTrigger(name);
         }
+
+        public void WeaponEnable()
+        {
+            WeaponCollider.enabled = true;
+        }
+
+        public void WeaponDisable()
+        {
+           WeaponCollider.enabled = false;
+        }
+        #endregion
     }
 
 }
